f49a3f138ccaedb25f3708fb9c3eb638ca8a134f
Prabodashan <prabodashan14@gmail.com>
2023-01-21 20:21:10 +0530

    completed UI

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { AuthContext } from "./../../context/authContext";
+import "./login.scss";
+
+const Login = () => {
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = () => {
+    login();
+  };
+
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eum
+            perspiciatis debitis porro, aut magnam voluptatem dolorem cupiditate
+            labore laborum, aperiam suscipit corporis qui possimus?
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" name="username" placeholder="Username" />
+            <input type="password" name="password" placeholder="Password" />
+            <button onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
